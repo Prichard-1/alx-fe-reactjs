cca69186e7bdbd1aa2a7a0364943a5aaf065c600
Prichard Dube <genarodube8@gmail.com>
2025-07-31 09:13:22 +0200

    add alx react app folder

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
-import viteLogo from '/vite.svg';
-import './App.css';
-import WelcomeMessage from './components/WelcomeMessage';
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <WelcomeMessage />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
