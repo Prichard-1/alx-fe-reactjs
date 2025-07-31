c95cce4a46e178a18a8fe1e19a9b7f02439b0a48
Prichard Dube <genarodube8@gmail.com>
2025-07-31 14:16:52 +0200

    add src

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,6 @@
-import UserProfile from './components/UserProfile';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
-
-function App() {
-  return (
-    <>
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-    </>
-  );
-}
-
-export default App;
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
