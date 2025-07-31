79f5d9ea304963f933030fbe250f9d4f5d01cbc6
Prichard Dube <genarodube8@gmail.com>
2025-07-31 09:34:18 +0200

    add alx react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,15 @@
 import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-      <WelcomeMessage />
       <MainContent />
       <Footer />
-    </div>
+    </>
   );
 }
 
