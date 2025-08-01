751f37ab38d407371cc88ab6e05e527228d304b8
Prichard Dube <genarodube8@gmail.com>
2025-08-01 09:11:28 +0200

    add a duplicate of alx-react-app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#e0f7fa', minHeight: '300px' }}>
+      <p>Explore the most beautiful cities around the world!</p>
+    </main>
+  );
+}
+
+export default MainContent;
+
+
