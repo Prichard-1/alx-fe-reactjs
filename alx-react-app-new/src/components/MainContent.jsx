2113cb68b29568188a8c3f7cd13520532f366caf
Prichard Dube <genarodube8@gmail.com>
2025-08-01 09:16:36 +0200

    add updated MainContent jsx file

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#e0f7fa', minHeight: '300px' }}>
-      <p>Explore the most beautiful cities around the world!</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
