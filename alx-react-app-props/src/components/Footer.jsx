c627ffc6eea497a48b4b14ac9130b83be1d547e9
Prichard Dube <genarodube8@gmail.com>
2025-08-02 22:32:19 +0200

    add alx react app props

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>&copy; 2025 My Travel Blog</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
+
