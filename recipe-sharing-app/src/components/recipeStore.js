8b9c91a47a8fea3dd9d6acb1cf3f019445f0e2d5
Prichard Dube <genarodube8@gmail.com>
2025-08-06 12:54:34 +0200

    added app components

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,11 +1,26 @@
 import { create } from 'zustand';
 
-export const useRecipeStore = create((set) => ({
+export const useRecipeStore = create(set => ({
   recipes: [],
-  addRecipe: (newRecipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, newRecipe],
-    })),
+
+  // Add a new recipe
+  addRecipe: (newRecipe) => set(state => ({
+    recipes: [...state.recipes, newRecipe],
+  })),
+
+  // Replace all recipes (bulk initialization or reset)
   setRecipes: (recipes) => set({ recipes }),
+
+  // Update a specific recipe by ID
+  updateRecipe: (updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(recipe =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
+
+  // Delete a recipe by ID
+  deleteRecipe: (id) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id),
+  })),
 }));
 
