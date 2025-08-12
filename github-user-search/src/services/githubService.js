2c609a96d2da465d9da1078536a2633c548445c0
Prichard Dube <genarodube8@gmail.com>
2025-08-12 21:36:02 +0200

    API intergration

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,4 +2,15 @@ const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
 headers: {
   Authorization: `token ${apiKey}`
 }
+// src/services/githubService.js
+import axios from 'axios';
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
