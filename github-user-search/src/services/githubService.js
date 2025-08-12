2064d9bd3ac2e281317afd35bdb2fa11e668a2de
pricharddube <genarodube8@gmail.com>
2025-08-12 22:50:15 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,17 @@
 // src/services/githubService.js
 import axios from 'axios';
 
-export const fetchUserData = async (username) => {
+export const fetchUserData = async (username, location, minRepos) => {
+  let query = '';
+
+  if (username) query += `${username}`;
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
+
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
+    const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+    return response.data.items;
   } catch (error) {
     throw error;
   }
 };
-
