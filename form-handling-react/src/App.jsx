4713da10918ec01d9412f55b8c2061e6fa129fac
Prichard Dube <genarodube8@gmail.com>
2025-09-08 14:18:30 +0200

    add App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,12 +1,12 @@
-import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/FormikForm";
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";  // Capitalize here
 
 function App() {
   return (
     <div className="p-6">
       <RegistrationForm />
       <hr className="my-6" />
-      <FormikForm />
+      <FormikForm />   {/* Capitalized usage */}
     </div>
   );
 }
