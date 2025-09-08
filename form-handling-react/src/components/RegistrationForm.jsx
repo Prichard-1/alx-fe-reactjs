dd95ce19351f6630b2656c74b8802d1da0420fa5
Prichard Dube <genarodube8@gmail.com>
2025-09-08 14:03:08 +0200

    fixed the registrationform

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -31,7 +31,6 @@ const RegistrationForm = () => {
     setErrors(validationErrors);
 
     if (Object.keys(validationErrors).length === 0) {
-      // Example mock API call
       try {
         const response = await fetch("https://jsonplaceholder.typicode.com/users", {
           method: "POST",
