74156cbc02b98c73e5db50a32cf4527a1fd3046a
Prichard Dube <genarodube8@gmail.com>
2025-09-09 13:55:25 +0200

    added blogpost file

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,14 +1,17 @@
 import { useParams } from "react-router-dom";
 
 const BlogPost = () => {
-  const { postId } = useParams();
+
+  const { id } = useParams();
+
   return (
     <div>
-      <h1>Blog Post: {postId}</h1>
-      <p>This page is dynamically rendered based on the post ID.</p>
+      <h1>Blog Post: {id}</h1>
+      <p>This page is dynamically rendered based on the blog post ID.</p>
     </div>
   );
 };
 
 export default BlogPost;
+
 
